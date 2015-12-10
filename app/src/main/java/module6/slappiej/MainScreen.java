46e2a512ffc1b013737cd78beab1482a5b2c7f51
Jan-Willem <janwillem.nh@gmail.com>
2015-12-10 14:16:11 +0100

    email?

--- app/src/main/java/module6/slappiej/MainScreen.java
+++ app/src/main/java/module6/slappiej/MainScreen.java
@@ -1,7 +1,11 @@
 package module6.slappiej;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+    }
+
+    public void gotoEmail(View view) {
+        Intent intent = new Intent(this, Email.class);
+        startActivity(intent);
     }
 }
